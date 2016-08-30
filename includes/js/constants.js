39adbc8bb60e70d93f0ccb6f018943a8aeb4e79a
Jordan Lee <jordan@transmissionbt.com>
2016-08-30 01:52:17 -0500

    static pages working for landing page, download page

---
+++ includes/js/constants.js
@@ -0,0 +1,8 @@
+var Constants = {
+    copyright_years: "2005 - " + new Date().getFullYear().toString(),
+    sha256_dmg: "926a878cac007e591cfcea987048abc0689d77e7729a28255b9ea7b73f22d693",
+    sha256_tar: "3a8d045c306ad9acb7bf81126939b9594553a388482efa0ec1bfb67b22acd35f",
+    current_version: "2.92"
+    // Uncomment this for beta versions 
+    // , beta_version: "2.60b1"
+}
--- includes/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-define( "TRANSMISSION_VERSION", "2.92" );
-define( "YEAR", "2005 - 2016" );
-define( "MAC_HASH",     "926a878cac007e591cfcea987048abc0689d77e7729a28255b9ea7b73f22d693");
-define( "tar_xz_HASH",  "3a8d045c306ad9acb7bf81126939b9594553a388482efa0ec1bfb67b22acd35f");
-// Uncomment this for beta versions 
-//define( "TRANSMISSION_BETA_VERSION", "2.60b1" );
-?>
