113a9febe8d1df8f272cfa605325a6c15e265b17
Mike Gelfand <mikedld@mikedld.com>
2017-02-15 23:56:48 +0300

    Add Windows build to downloads, marked as "early preview"
    Fixes: #5

--- includes/js/constants.js
+++ includes/js/constants.js
@@ -2,6 +2,8 @@ var Constants = {
     copyright_years: "2005 - " + new Date().getFullYear().toString(),
     sha256_dmg: "926a878cac007e591cfcea987048abc0689d77e7729a28255b9ea7b73f22d693",
     sha256_tar: "3a8d045c306ad9acb7bf81126939b9594553a388482efa0ec1bfb67b22acd35f",
+    sha256_msi32: "f8bfaba603036d35f74bbd5d27ea5bc51fe58d991af48228c290323e406bef00",
+    sha256_msi64: "8cee6436e79d48409d76a85bbb1bb506c8c76c8f686551b9e7e13b9a2ca2299d",
     current_version: "2.92"
     // Uncomment this for beta versions 
     // , beta_version: "2.60b1"
