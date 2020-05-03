ee1d74858bff96ae06b91e4f1f8c1f77d85279bf
Charles Kerr <ckerr@github.com>
2020-05-03 18:58:07 -0500

    chore: update Constants dmg vars

--- includes/js/constants.js
+++ includes/js/constants.js
@@ -1,11 +1,11 @@
 var Constants = {
     copyright_years: "2005 - " + new Date().getFullYear().toString(),
-    sha256_dmg: "2cae915ae0e37fc5983406ca7fbd53a054a7153d3bfd7a6cef117a8a28d8a78a",
+    sha256_dmg: "1b4a641739d3b1daa4efdfe6b072f01407be317f14f7c2a464d9787d82e68550",
     sha256_tar: "423afd8e20912e62ed5e1f81382a12f0e69618930965497f3b3444a0e0e37b3b",
     sha256_msi32: "9083bed824d707e8791f53811aaa88def8047d40f221a8e823571c103cf3aff6",
     sha256_msi64: "a35672252661f16382072fd0ae87b8eafd47f5535439336fe2f161a542f9a1ab",
     current_version: "3.00",
-    current_version_dmg: "2.94",
+    current_version_dmg: "3.00",
     current_version_msi: "2.94",
     current_version_tar: "3.00",
     min_req_dmg: "Mac OS X 10.10 or later",
