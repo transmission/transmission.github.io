6a21cecf02cc400ab11a026a9e0daba02b874c5d
Mike Gelfand <mikedld@mikedld.com>
2020-05-22 14:12:08 +0300

    Update tarball hash

--- includes/js/constants.js
+++ includes/js/constants.js
@@ -1,7 +1,7 @@
 var Constants = {
     copyright_years: "2005 - " + new Date().getFullYear().toString(),
     sha256_dmg: "1b4a641739d3b1daa4efdfe6b072f01407be317f14f7c2a464d9787d82e68550",
-    sha256_tar: "423afd8e20912e62ed5e1f81382a12f0e69618930965497f3b3444a0e0e37b3b",
+    sha256_tar: "9144652fe742f7f7dd6657716e378da60b751aaeda8bef8344b3eefc4db255f2",
     sha256_msi32: "eeab85327fa8a1299bb133d5f60f6674ca9e76522297202bbe39aae92dad4f32",
     sha256_msi64: "c34828a6d2c50c7c590d05ca50249b511d46e9a2a7223323fb3d1421e3f6b9d1",
     current_version: "3.00",
